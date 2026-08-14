2f3ab210f63cfbf615cf912b3d8adf73d8935b57
ZiyuMeng98 <154886834+ZiyuMeng98@users.noreply.github.com>
2026-08-14 17:17:56 +0800

    Update main.sh

--- BC cancer/main.sh
+++ BC cancer/main.sh
@@ -4,9 +4,8 @@ bash cellranger.sh 2>&1 | tee -a cellranger.log
 # R语言Seruat库
 library(Seurat)
 library(ggplot2)
-
 set.seed(1234)
-
+# 输入文件
 meta <- data.frame(
   sample_id = c("SRR12603790", "SRR12603789", "SRR12603787","SRR12603786", "SRR12603785", "SRR12603784","SRR12603783", "SRR12603782", "SRR12603781","SRR12603780", "SRR12603788"),
   patient = c("P01", "P02", "P03", "P04", "P05", "P06","P07", "P08", "N01", "N02", "N03"),
@@ -15,6 +14,7 @@ meta <- data.frame(
 dir.create("03.seurat", showWarnings = FALSE)
 objects <- list()
 qc_summary <- data.frame()
+
 # 读取、质控、标准化和高变基因筛选
 for (i in seq_len(nrow(meta))) {
   sid <- meta$sample_id[i]
@@ -34,3 +34,23 @@ for (i in seq_len(nrow(meta))) {
   qc_summary <- rbind(qc_summary,data.frame(sample_id = sid,patient = meta$patient[i],tissue = meta$tissue[i],cells_before = cells_before,cells_after = cells_after,retained_percent = round(100 * cells_after / cells_before,2),median_UMI = median(obj$nCount_RNA),median_genes = median(obj$nFeature_RNA),median_percent_mt = median(obj$percent.mt))
   )
 }
+# 保存质控、标准化结果
+saveRDS(objects,"03.seurat/all_samples_qc_norm.rds")
+write.csv(qc_summary,"03.seurat/QC_summary.csv",row.names = FALSE)
+
+# 选择3000个用于进一步分析的基因
+features <- SelectIntegrationFeatures(object.list = objects,nfeatures = 3000)
+length(features) # [1] 3000
+head(features) # 显示基因名
+# 寻找样本间的anchors（主要用来识别和消除批次效应，或实现跨模态（如 RNA 与 ATAC）的数据对齐）
+options(future.globals.maxSize = 8 * 1024^3)
+anchors <- FindIntegrationAnchors(object.list = objects , anchor.features = features , dims = 1:30)
+# 整合所有样本
+bladder <- IntegrateData(anchorset = anchors, dims = 1:30)
+# 检查整合结果
+bladder
+DefaultAssay(bladder)
+table(bladder$sample_id)
+table(bladder$tissue)
+# 保存整合对象
+saveRDS(bladder,"03.seurat/bladder_integrated_raw.rds")
